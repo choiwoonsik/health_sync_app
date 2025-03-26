d00fdbdda458969126b4229d6991a982af5c07c8
groom-siki <groom.siki@kakaostyle.com>
2025-03-26 18:17:57 +0900

    feat: 코드 및 패키지 정리

--- src/main/java/com/kbhealthcare/ocare/healthSync/kafka/consumer/HealthSyncConsumer.java
+++ src/main/java/com/kbhealthcare/ocare/healthSync/kafka/consumer/HealthSyncConsumer.java
@@ -1,4 +1,4 @@
-package com.kbhealthcare.ocare.healthSync.consumer;
+package com.kbhealthcare.ocare.healthSync.kafka.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
